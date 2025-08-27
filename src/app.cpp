981d7b60fb577e512540e6aa326f8fa6e4c6938c
SANeX15 <sanshraymchutke@gmail.com>
2025-08-27 12:40:10 +0530

    Mod Code 2

--- src/app.cpp
+++ src/app.cpp
@@ -4,7 +4,12 @@
 wxIMPLEMENT_APP(CRM_App);
 
 bool CRM_App::OnInit(){
+  // Create a new instance of the MainMenuFrame
   MainMenuFrame * mmf = new MainMenuFrame("CIAREM");
-  mmf->Show();
+
+  // Show the main window. This is the critical step to avoid the segmentation fault.
+  mmf->Show(true);
+
+  // Return true to indicate that the application should start its main event loop
   return true;
 }
