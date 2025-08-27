4dee5a07d852adb02c6b1f8a9c1256453f8d18a6
SANeX15 <sanshraymchutke@gmail.com>
2025-08-27 21:40:15 +0530

    sql changes

--- src/db/db.cpp
+++ src/db/db.cpp
@@ -1,22 +1,46 @@
+#include <iostream>
 #include <mariadb/conncpp.hpp>
 #include "../libs/db-conf.h"
 
-sql::Driver * driver;
-sql::SQLString url("jdbc:mariadb://localhost:3306/" + dbCred::getDbName());
-sql::Properties props({{"user", "ciarem"},{"password", "151108"}});
+// A namespace to encapsulate the database logic
+namespace db {
 
+// The connect function now returns a shared pointer to a Connection object.
+// Using a shared pointer allows the connection to be shared safely among
+// multiple parts of the application.
+std::shared_ptr<sql::Connection> connect() {
+    try {
+        // We get the driver instance right before we need it. This avoids
+        // potential issues with global object initialization order.
+        sql::Driver *driver = sql::mariadb::get_driver_instance();
 
-bool connect(){
-  try {
-    if (driver != NULL){
-      driver = sql::mariadb::get_driver_instance();
+        // We create the connection options map on the stack.
+        sql::ConnectOptionsMap connectionOptions;
+        connectionOptions["host"] = "localhost";
+        connectionOptions["port"] = "3306";
+        connectionOptions["user"] = dbCred::getUser();
+        connectionOptions["password"] = dbCred::getPwd();
+        connectionOptions["db"] = dbCred::getDbName();
+
+        // The connect function returns a unique_ptr. We convert it to a shared_ptr
+        // and return it.
+        return std::shared_ptr<sql::Connection>(driver->connect(connectionOptions));
     }
-    std::unique_ptr<sql::Connection> conn(driver->connect(url, props));
-    return true;
-  }
-  catch (sql::SQLException & e){
-    return false;
-  }
+    catch (sql::SQLException & e){
+        std::cerr << "SQLException: " << e.what() << std::endl;
+        std::cerr << "Error code: " << e.getErrorCode() << std::endl;
+        return nullptr;
+    }
 }
 
-bool disconnect(){}
+// The disconnect function is no longer needed since a shared_ptr
+// will automatically close the connection when it goes out of scope.
+// However, if you were using a raw pointer, you would implement it here.
+void disconnect(std::shared_ptr<sql::Connection>& conn) {
+    if (conn && conn->isClosed() == false) {
+        conn->close();
+    }
+    // The shared_ptr will handle memory cleanup.
+}
+
+} // namespace db
